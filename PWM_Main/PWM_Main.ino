df6a812006f1c79cfc52ff53454c0f7e38979797
SubToxic101 <86693701+SubToxic101@users.noreply.github.com>
2022-02-22 14:11:50 -0500

    Update PWM_Main.ino

--- PWM_Main/PWM_Main.ino
+++ PWM_Main/PWM_Main.ino
@@ -1,13 +1,18 @@
-
+#include <Encoder.h>
 int pwmPin1 = A4;
 int pwmPin2 = A5;
 int dirPin = A4;
-int inputPin = A3;
+int inputPin1 = A2;
+int inputPin2 = A3;
+
+Encoder motor(inputPin1, inputPin2);
+
 int dirFlag = 0;
 void setup() {
   pinMode(pwmPin1, OUTPUT);
   pinMode(pwmPin2, OUTPUT);
-  pinMode(inputPin, INPUT);   
+  pinMode(inputPin1, INPUT); 
+  pinMode(inputPin2, INPUT);  
 }
 
 void loop() {
@@ -21,4 +26,17 @@ void loop() {
   }
 
   analogWrite(dirPin, 128);
+
+  int pos = motor.Read();
+  Serial.print(pos);
+  //delay(2000);
+
+  /*
+  if (dirFlag == 0) {
+    dirFlag = 1;
+  }
+  else if (dirFlag == 1) {
+    dirFlag = 0;
+  }
+  */
 }
