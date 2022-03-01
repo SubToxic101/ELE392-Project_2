feaa4d57c28a426b5a46c24f97d543fa66929f02
SubToxic101 <86693701+SubToxic101@users.noreply.github.com>
2022-03-01 15:26:24 -0500

    3/1 Changes

--- PWM_Main/PWM_Main.ino
+++ PWM_Main/PWM_Main.ino
@@ -8,12 +8,23 @@ float pos = 0.0;
 float rpm = 0.0;
 float rpm_counter = 0.0;
 float rpm_start = 0.0;
+float set_rpm = 0.0;
 int ms = 0;
 int ms_end = 0;
+float Kp = 0.0;
+float Ki = 0.0;
+float Ts = 0.0;
+float y_k = 0.0;
+float y_k1 = 0.0;
+float x_k = 0.0;
+float x_k1 = 0.0;
+float pwm = 0.0;
+
+
 
 Encoder myEnc(inputPin1, inputPin2);
 
-int dirFlag = 0;
+int dirFlag = 1;
 void setup() {
   pinMode(pwmPin1, OUTPUT);
   pinMode(pwmPin2, OUTPUT);
@@ -30,50 +41,72 @@ void loop() {
     dirPin = A4;
   }
 
-  
+  //Kp = 0.236;
+  Kp = 4.65;
+  Ki = 105;
+  set_rpm = 60;
+  Ts = 0.05;
 
-  pos = myEnc.read() * (360)/(4.4*48);
+
+  pos = myEnc.read() * (360) / (4.4 * 48);
   //Serial.print("degrees: ");
   //Serial.println(pos);
 
   rpm = 0;
   rpm_start = myEnc.read();
   ms = millis();
-  ms_end = ms+50;
-  while(ms<ms_end) {
+  ms_end = ms + 50;
+  while (ms < ms_end) {
+    //Serial.println("while called");
     rpm_counter = myEnc.read();
     ms = millis();
-    
+
     /*Serial.print("rpmcounter: ");
-    Serial.println(rpm_counter);
-    Serial.print("ms: ");
-    Serial.println(ms);
+      Serial.println(rpm_counter);
+      Serial.print("ms: ");
+      Serial.println(ms);
     */
   }
-  rpm = (rpm_counter-rpm_start) * (1000*60)/(4.4*48*50);
+  rpm = (rpm_counter - rpm_start) * (1000 * 60) / (4.4 * 48 * 50);
   //Serial.print("rpm: ");
   //Serial.println(rpm);
-  
-  if (pos < 720) {
-    analogWrite(dirPin, 255);
-  }
-  else {
-    analogWrite(dirPin, 0);
-  }
 
+  /*
+    if (pos < 720) {
+    analogWrite(dirPin, 255);
+    }
+    else {
+    analogWrite(dirPin, 0);
+    }
+  */
+  
+  x_k = set_rpm - rpm;
+  pwm = (int)(Kp * x_k - Kp * x_k1 + Ki * Ts * x_k1 + y_k1);
+  x_k1 = x_k;
+  y_k1 = y_k;
+
+  if (pwm > 255) {
+    pwm = 255;
+  }
+  else if (pwm < 0) {
+    pwm = 0;
+  }
+  Serial.print("pwm: ");
+  Serial.println(pwm);
+  analogWrite(dirPin, pwm);
   Serial.print(rpm);
   Serial.print(",");
   Serial.println(millis());
-  
-  
-  
+
+
+
 
   /*
-  if (dirFlag == 0) {
+    if (dirFlag == 0) {
     dirFlag = 1;
-  }
-  else if (dirFlag == 1) {
+    }
+    else if (dirFlag == 1) {
     dirFlag = 0;
-  }
+    }
   */
 }
